747f7d0e853c05ffb6f48c3849a65ba34aa33d4b
Leon Ronchi <leonbronchi@gmail.com>
2023-10-02 10:46:53 -0300

    tentando atualizar quantidade de itens no carrinho tentativa 3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import Cabecalho from "./templates/Cabecalho";
 import { useEffect, useState } from "react";
 
 function App() {
+  const [quantidadeCarrinho, setQuantidadeCarrinho] = useState(0);
+  const [carrinho, setCarrinho] = useState([]);
+  const atualizarQuantidadeCarrinho = (quantidade, produto) => {
+    setQuantidadeCarrinho(quantidadeCarrinho + quantidade);
+    setCarrinho([...carrinho, produto]);
+  };
+
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((resposta) => resposta.json())
@@ -17,7 +24,8 @@ function App() {
     <div className="App">
       <Cabecalho/>
       <BarraBusca/>
-      <GradeProdutos listaProdutos={produtos}/>
+      <GradeProdutos listaProdutos={produtos}
+      atualizarQuantidadeCarrinho={atualizarQuantidadeCarrinho}/>
     </div>
   );
 }
